a51f41570ca057f056b0090ddf924a83890383df
Jon Baclig <jon@baclig.com>
2017-10-25 12:50:17 -0700

    add List component

---
+++ src/Components/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import List from '../List';
+import logo from '../../logo.svg';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">To-Do List</h1>
+        </header>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/List/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class List extends Component {
+    render() {
+        return (
+            <div className="List">
+                This is a to-do-list
+            </div>
+        );
+    }
+}
+
+export default List;
